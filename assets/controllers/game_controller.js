7e26e5a26e56a49e59be9cd01c57da7e8c2b12fd
mathysfrc <mathysf41@gmail.com>
2023-12-01 15:19:18 +0100

    jeu en symfony

---
+++ assets/controllers/game_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from '@hotwired/stimulus';
+import { getComponent } from '@symfony/ux-live-component';
+
+/*
+ * This is an example Stimulus controller!
+ *
+ * Any element with a data-controller="hello" attribute will cause
+ * this controller to be executed. The name "hello" comes from the filename:
+ * hello_controller.js -> "hello"
+ *
+ * Delete this file or adapt it for your use!
+ */
+export default class extends Controller {
+
+    async initialize() {
+        this.component = await getComponent(this.element);
+    }
+    moveLeft()
+    {
+        this.component.action('moveLeft');
+    }
+
+    moveRight()
+    {
+        this.component.action('moveRight');
+    }
+}
